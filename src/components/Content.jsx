4a93c5bc0e911825c4ab0e41bb30c5f2ee8e7ceb
Sergey <dzzisperfect@gmail.com>
2021-07-29 22:51:06 +0300

    react-1

---
+++ src/components/Content.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+      <div className='content'>
+        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+        <div>ava+descr</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>
+            <div>post 1 </div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Content;
+
+    
