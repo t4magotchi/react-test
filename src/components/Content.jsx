10f86a6c5da82a551ee4f1a8c1473a41183966b7
Sergey <dzzisperfect@gmail.com>
2021-08-01 21:40:30 +0300

    css_module

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import classes from './Content.module.css';
 const Content = () => {
     return (
-      <div className='content'>
+      <div className={classes.content}>
         <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
         <div>ava+descr</div>
         <div>
           my posts
           <div>new post</div>
           <div>
-            <div>post 1 </div>
+            <div className='item'>post 1 </div>
             <div>post 2</div>
           </div>
         </div>
