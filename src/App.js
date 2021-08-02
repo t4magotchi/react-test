7a4e89c273215caf94f7d953f8458584515b116b
Sergey <dzzisperfect@gmail.com>
2021-08-02 23:38:46 +0300

    New-components1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Content from "./components/Content";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import Content from "./components/Content/Content";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
