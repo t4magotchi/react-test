c911fe1a910c766bac473138854730fd01708176
Sergey <dzzisperfect@gmail.com>
2021-08-03 23:18:03 +0300

    Component-Dialogs

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Content />
-     </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                {/*<Content />*/}
+                <Dialogs/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
