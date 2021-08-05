4c1924e927f18f459d6fce3cb23d157bf3f6785e
Sergey <dzzisperfect@gmail.com>
2021-08-05 20:43:15 +0300

    dialog-style1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,24 @@ import classes from "./Dialogs.module.css"
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
-            Dialogs
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Sergey</div>
+                <div className={classes.dialog}>Ayaal</div>
+                <div className={classes.dialog}>Yan</div>
+                <div className={classes.dialog}>Alexander</div>
+                <div className={classes.dialog}>Roman</div>
+            </div>
+            <div className={classes.messages}>
+                <div>
+                    Hi
+                </div>
+                <div>
+                    How are you?
+                </div>
+                <div>
+                    Whatsapp man
+                </div>
+            </div>
         </div>
     )
 }
